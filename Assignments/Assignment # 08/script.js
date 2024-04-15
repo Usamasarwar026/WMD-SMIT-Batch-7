193513b796a03be1822d917d1e6f96ff7d173e5e
usamasarwar026 <usamasarwar160492@gmail.com>
2024-04-15 10:53:02 +0500

    JS

--- Assignments/Assignment # 08/script.js
+++ Assignments/Assignment # 08/script.js
@@ -55,3 +55,27 @@
 //     alert(base + " raised to the power of " + exponent + " is:" + result);
 // }
 
+
+
+
+// 5. Write a JS code to print a pattern using for loop
+/* 
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+1 2 3 4 5 6
+1 2 3 4 5 6 7
+1 2 3 4 5 6 7 8 
+*/
+
+
+// for (let i = 1; i <= 8; i++) {
+//     var p = ' '
+//     for (let j = 1; j <= i; j++){
+//         p += j + ' ';
+//     }
+//     console.log(p);
+// }
+
---
+++ Assignments/Assignment # 08/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+
+// for (let i = 1; i <= 8; i++) {
+//     let pattern = ''; // Initialize an empty string to store the pattern for each row
+//     for (let j = 1; j <= i; j++){
+//         pattern += j + ' '; // Concatenate each number followed by a space
+//     }
+//     console.log(pattern); // Print the pattern for each row
+// }
