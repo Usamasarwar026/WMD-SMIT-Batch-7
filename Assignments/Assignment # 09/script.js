f4149d1dbbce892ced04af024a5bf1a7975a4cb2
usamasarwar026 <usamasarwar160492@gmail.com>
2024-05-02 22:34:15 +0800

    JS

---
+++ Assignments/Assignment # 09/script.js
@@ -0,0 +1,48 @@
+
+
+// a) Return the length of a given string.
+
+
+// function count_length(count){
+//     return count.length;
+// }
+// var name = "USAMA SARWAR";
+// let length = count_length(name);
+// console.log("The length of the string is", length);
+
+
+// b) Concatenate two strings together.
+
+// var Fname = "USAMA"
+// var Lname = "SARWAR"
+
+// function Concatenate(add_string){
+//          add_string = Fname + " " + Lname ;
+//         return add_string;
+// }
+// let concatenating = Concatenate(Fname + Lname)
+
+// console.log("After Concatenating the string is", concatenating);
+
+
+// c) Determine if a given string is empty.
+
+// function check_string(str){
+//     if(str == ""){
+//         console.log("string is empty");
+//     }
+//     else{
+//         console.log("string is not empty")
+//     }
+//     return str;
+// }
+// let string = "USAMA";
+// check_string(string);
+
+
+// d) Count the number of vowels in a string.
+
+
+function check_vowel(str){
+    
+}
