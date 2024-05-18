0efcfdd138e96b90c03bbc202f693885eded3472
usamasarwar026 <usamasarwar160492@gmail.com>
2024-05-19 02:37:21 +0500

    JS

--- Assignments/Assignment # 11/script.js
+++ Assignments/Assignment # 11/script.js
@@ -1,15 +1,77 @@
 
     // --------------------------- Javascript Arrays -----------------------
 
-    var number = [1,2,3,4,5,6,7,8,9];
+    // var number = [1,2,3,4,5,6,7,8,9];
 
     //  console.log(number.at(8));
-
-    //  console.log(number.push(6));
+    
+    // number.push(6)
     //  console.log(number);
 
-    // console.log(number.pop());
+    // number.pop()
     // console.log(number);
+
+    // number.fill(2)
+    // console.log(number);
+
+    // number.shift();
+    // console.log(number);
+
+    // number.unshift(0);
+    // console.log(number);
+
+    // number.reverse();
+    // console.log(number);
+    
+    // var number = ['A','Z','G','B','U'];
+    // number.sort();
+    // console.log(number);
+
+
+    // first method to concatenate the Array
+
+    // var First_array = ["Ali", "Khalid", "Habibullah"];
+    // var Second_array = ["Awais", "Tayyab",];
+    // var third_array = First_array.concat(Second_array);
+    // console.log(third_array);
+
+
+     // second method to concatenate the Array
+
+    //  var First_array = ["Ali", "Khalid", "Habibullah"];
+    //  var Second_array = First_array.concat("Awais", "Tayyab");
+    //  console.log(Second_array);
+
+
+    // convert the whole/full array into String
+    // var array = ["Tayyab", "Awais", "Umar"];
+    // var convert_to_string = array.join(" ");
+    // console.log(convert_to_string);
+
+
+    // search the item in the Array
+    // var array = ["Ali", "Khalid", "Habibullah"];
+    // var search_item = array.includes("Habibullah")
+    // console.log(search_item)
+
+
+    // make the new array from the existing array
+
+    // var array = ["Tayyab","Awais","Usama","Danish","Abdullah","Ali","yousaf"]
+    // var new_array = array.slice(2,6);
+    // console.log(new_array)
+    
+    
+    // use the splice methode we can add or remove item from the array
+    // // Add item in array
+    // var array = ["Tayyab","Awais","Usama","Danish","Abdullah","Ali","yousaf"];
+    // array.splice(1,0,"Mustafa","Hamza","Rehman");
+    // console.log(array);
+    
+    // // remove item in the array
+    // var array = ["Tayyab","Awais","Usama","Danish","Abdullah","Ali","yousaf"];
+    // array.splice(1,3);
+    // console.log(array);
 
 
 
