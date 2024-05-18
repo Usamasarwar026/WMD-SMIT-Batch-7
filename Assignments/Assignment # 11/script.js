5c320d07936fd670646f13c3aac84e31656645b3
usamasarwar026 <usamasarwar160492@gmail.com>
2024-05-18 08:37:25 +0500

    JS

---
+++ Assignments/Assignment # 11/script.js
@@ -0,0 +1,16 @@
+
+    // --------------------------- Javascript Arrays -----------------------
+
+    var number = [1,2,3,4,5,6,7,8,9];
+
+    //  console.log(number.at(8));
+
+    //  console.log(number.push(6));
+    //  console.log(number);
+
+    // console.log(number.pop());
+    // console.log(number);
+
+
+
+     
