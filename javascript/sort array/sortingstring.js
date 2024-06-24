428a8a86caed50dc3ea6ed82d2160585085e39c7
usamasarwar026 <usamasarwar160492@gmail.com>
2024-06-24 10:12:32 +0500

    JS

---
+++ javascript/sort array/sortingstring.js
@@ -0,0 +1,46 @@
+
+// let strs = [club,clue,cluing,clup]
+
+// let arraysort = function(str){
+//     Array.sort(str);
+//     let first = str[0];
+//     let last = str[str.length-1];
+//     index = 0;
+//     while(index < first.length())
+//         if (first.charAt(index) == last.charAt(index)) {
+//             index++    
+//         }
+//         else{
+//             break;
+//         }
+//         return index == 0 ? '':first.substring(0,index)
+
+
+// }
+
+
+// console.log(arraysort);
+
+
+
+
+let strs = ['club', 'clue', 'cluing', 'zlw'];
+
+let arraysort = function(str){
+    str.sort();
+    let first = str[0];
+    let last = str[str.length - 1];
+    let index = 0;
+
+    while (index < first.length) {
+        if (first.charAt(index) == last.charAt(index)) {
+            index++;
+        } else {
+            break;
+        }
+    }
+
+    return index == 0 ? 'there is not longest prefix' : first.substring(0, index);
+}
+
+console.log(arraysort(strs)); // This will print the result of arraysort
