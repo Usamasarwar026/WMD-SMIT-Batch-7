27fb289bac5fe78621cd676a6288182f0eb788cc
usamasarwar026 <usamasarwar160492@gmail.com>
2024-07-31 17:06:29 +0500

    JS

---
+++ javascript/javascript advance/loops.js
@@ -0,0 +1,44 @@
+
+
+
+// -----------while loop----------
+
+// let a = 0;
+// while(a <= 20){
+//     if(a%2 == 0){
+//         console.log(`${a} is an even number`);
+//     }
+//     else{
+//         console.log(`${a} is an odd number`);
+//     }
+//     a++
+// }
+
+
+
+// --------------for loop--------------
+
+
+// for (let index = 0; index < 20; index++) {
+//     if (index % 2 == 0) {
+//         console.log(index + " this is an even number")
+//     } else {
+//         console.log(index + " this is an odd number")
+//     }
+    
+// }
+
+
+
+
+// let array = [1,3,5,6,8,13,43,45,87,89,98,99];
+
+// for(let i=0; i <= array.length; i++){
+//     if(array[i] % 2 == 0){
+//         console.log(array[i] + " is an even number");
+//     }
+//     else{
+//         console.log(array[i] + " is not an even number");
+//     }
+// }
+
---
+++ javascript/javascript advance/restoperator.js
@@ -0,0 +1,12 @@
+
+
+let arr = [1,2,3,4,5]
+function cal(arr){
+    let sum = 0;
+    for(let i in arr)
+        sum += arr[i];
+    return sum;
+}
+
+let result = cal(arr);
+console.log(result);
